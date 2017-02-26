b966878fb12bc846ca4e1d3be0c18f49664bcfa4
stlyx <stlyx@qq.com>
2017-02-27 01:23:20 +0800

    game prototype

--- js/app.js
+++ js/app.js
@@ -24,6 +24,8 @@ var canvas = $('#canvas');
 var canvasCtx = document.getElementById("canvas").getContext("2d");
 var appWidth = canvas.parent().width();
 canvas.attr('width', appWidth);
+WIDTH = canvas.width();
+HEIGHT = canvas.height();
 var drawVisual;
 
 //main block for doing the audio recording
@@ -40,8 +42,7 @@ if (navigator.getUserMedia) {
         function (stream) {
             source = audioCtx.createMediaStreamSource(stream);
             source.connect(analyser);
-
-            visualize();
+            $('#stopBtn').hide();
         },
 
         // Error callback
@@ -53,9 +54,129 @@ if (navigator.getUserMedia) {
     console.log('getUserMedia not supported on your browser!');
 }
 
+var player;
+var enemies = [];
+var score;
+var gameArea = {
+    start: function () {
+        this.frameNo = 0;
+    },
+    clear: function () {
+        canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+    }
+};
+analyser.fftSize = 32;
+var bufferLength = analyser.frequencyBinCount;
+var dataArray = new Uint8Array(bufferLength);
+
+function updateGameArea() {
+    drawVisual = requestAnimationFrame(updateGameArea);
+    for (var e1 in enemies) {
+        if (player.crashWith(enemies[e1])) {
+            stopGame();
+            return;
+        }
+    }
+    gameArea.clear();
+    gameArea.frameNo++;
+    $('#score').text(gameArea.frameNo);
+
+    analyser.getByteFrequencyData(dataArray);
+    var avg = 0;
+    for (var i = 0; i < bufferLength; i++) {
+        avg += dataArray[i];
+    }
+    avg = avg / bufferLength;
+    if (avg > 10) {
+        player.speedX = 2;
+    } else {
+        player.speedX = 0;
+    }
+    if (avg > 30 && player.hitBottom()) {
+        player.speedY = Math.max(-avg / 7, -7);
+        console.log('jump ' + player.speedY);
+    }
+    player.update();
+
+    if (gameArea.frameNo % 300 === 1) {
+        enemy = new component(40, 40, 'red', 1000, HEIGHT - 40);
+        enemy.speedX = -1;
+        enemies.push(enemy);
+    }
+    for (var e2 in enemies) {
+        enemies[e2].update();
+    }
+
+}
+
+function component(width, height, color, x, y, type) {
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.gravity = 0.08;
+    this.x = x;
+    this.y = y;
+    this.update = function () {
+        this.speedY += this.gravity;
+        this.x += this.speedX;
+        this.y += this.speedY;
+        this.hitBottom();
+        canvasCtx.fillStyle = color;
+        canvasCtx.fillRect(this.x, this.y, this.width, this.height);
+    };
+    this.hitBottom = function () {
+        var btmPos = HEIGHT - this.height;
+        if (this.y > btmPos) {
+            this.y = HEIGHT - this.height;
+            this.speedY = 0;
+            return true;
+        } else if (this.y === btmPos) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+    this.crashWith = function (otherobj) {
+        var myleft = this.x;
+        var myright = this.x + (this.width);
+        var mytop = this.y;
+        var mybottom = this.y + (this.height);
+        var otherleft = otherobj.x;
+        var otherright = otherobj.x + (otherobj.width);
+        var othertop = otherobj.y;
+        var otherbottom = otherobj.y + (otherobj.height);
+        var crash = true;
+        if ((mybottom < othertop) ||
+            (mytop > otherbottom) ||
+            (myright < otherleft) ||
+            (myleft > otherright)) {
+            crash = false;
+        }
+        return crash;
+    };
+}
+
+$('#startBtn').click(startGame);
+
+$('#stopBtn').click(stopGame);
+
+function startGame() {
+    $('#stopBtn').show();
+    $('#startBtn').hide();
+    player = new component(60, 100, 'blue', 10, HEIGHT - 100);
+    enemies = [];
+    gameArea.start();
+    updateGameArea();
+}
+
+function stopGame() {
+    $('#stopBtn').hide();
+    $('#startBtn').show();
+    cancelAnimationFrame(drawVisual);
+}
+
 function visualize() {
-    WIDTH = canvas.width();
-    HEIGHT = canvas.height();
     analyser.fftSize = 32;
     var bufferLength = analyser.frequencyBinCount;
     var dataArray = new Uint8Array(bufferLength);
@@ -70,7 +191,7 @@ function visualize() {
         canvasCtx.fillStyle = 'rgb(0, 0, 0)';
         canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
 
-        var barWidth = (WIDTH / bufferLength) * 2.5;
+        var barWidth = (WIDTH / bufferLength);
         var barHeight;
         var x = 0;
         var avg = 0;
@@ -80,7 +201,7 @@ function visualize() {
             avg += dataArray[i];
 
             canvasCtx.fillStyle = 'rgb(' + (barHeight + 100) + ',50,50)';
-            canvasCtx.fillRect(x, HEIGHT - barHeight / 2, barWidth, barHeight / 2);
+            canvasCtx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
 
             x += barWidth + 1;
         }
